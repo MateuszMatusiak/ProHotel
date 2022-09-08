43a6c69b8f95edb4ddc8b941c4def7f3023c5ac9
Konrad Basta <86340967+Mafia193@users.noreply.github.com>
2022-09-09 00:55:37 +0200

    Added RoomView

--- ProHotel/Data/RoomEntryService.cs
+++ ProHotel/Data/RoomEntryService.cs
@@ -23,4 +23,11 @@ public class RoomEntryService
         }
         return Task.FromResult(res);
     }
+    
+    public Task<Room> GetRoom(int id)
+    {
+        string json = new HttpClient().GetStringAsync($"{Url}({id})").Result;
+        var roomsJson = JsonSerializer.Deserialize<RoomJson>(json) ?? new RoomJson();
+        return Task.FromResult(new Room(roomsJson));
+    }
 }
