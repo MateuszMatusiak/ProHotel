350d15159ae910b21edc0112b4a0e4854b9c7712
Piotr Bednarek <specjunior@gmail.com>
2022-09-09 02:53:08 +0200

    Added addons, formated RoomEntry

--- ProHotel/Data/Room.cs
+++ ProHotel/Data/Room.cs
@@ -2,24 +2,30 @@
 
 public class Room
 {
-	public string ImageUrl {get; set;}
-	public string Name {get; set;}
-	public float Space {get; set;}
-	public int Seats {get; set;}
-	public int Beds {get; set;}
-	public string Variant {get; set;}
-	public float PricePerDay {get; set;}
-    public Addons Addons { get; set; }
+	public string ImageUrl { get; set; }
+	public string RoomNumber { get; set; }
+	public float Space { get; set; }
+	public int Beds1Person { get; set; }
+	public int Beds2Person { get; set; }
+	public int BedsChildren { get; set; }
+	public int Beds { get; set; }
+	public string Variant { get; set; }
+	public float PricePerDay { get; set; }
+	public List<string> Addons { get; set; }
 
-    public Room(RoomJson roomJson)
-    {
-        ImageUrl = roomJson.images?[0].image_content ?? "";
-        Name = roomJson.room_number?.ToString() ?? "";
-        Space = roomJson.area ?? 0;
-        Seats = roomJson.bed_count_1p ?? 0;
-        Beds = roomJson.bed_count_2p ?? 0;
-        Variant = roomJson.variant ?? "";
-        PricePerDay = roomJson.price ?? 0;
-        Addons = roomJson.addons ?? new Addons();
-    }
+	public Room(RoomJson roomJson)
+	{
+		if(roomJson == null) return;
+
+		ImageUrl = roomJson.images?[0].image_content ?? "";
+		RoomNumber = roomJson.room_number?.ToString() ?? "";
+		Space = roomJson.area ?? 0;
+		Beds1Person = roomJson.bed_count_1p ?? 0;
+		Beds2Person = roomJson.bed_count_2p ?? 0;
+		BedsChildren = roomJson.bed_count_c ?? 0;
+		Beds = Beds1Person + Beds2Person;
+		Variant = roomJson.variant ?? "";
+		PricePerDay = roomJson.price ?? 0;
+		Addons = roomJson.addons?.GetAddons() ?? new List<string>();
+	}
 }
