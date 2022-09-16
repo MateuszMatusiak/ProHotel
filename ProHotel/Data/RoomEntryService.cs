afe39992e4df23c2ca3e3ded2f3bf01740fe44c7
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-09-16 20:26:20 +0200

    Poprawione wyszukiwanie

--- ProHotel/Data/RoomEntryService.cs
+++ ProHotel/Data/RoomEntryService.cs
@@ -4,31 +4,31 @@ namespace ProHotel.Data;
 
 public class RoomEntryService
 {
-    const string Url = "http://localhost:8080/v1/rooms";
-    const string UrlRandom = "http://localhost:8080/v1/rooms_random";
-	
-    public Task<Room[]> GetRooms(bool random = false)
-    {
-        string json = new HttpClient().GetStringAsync(random ? UrlRandom : Url).Result;
-        
-        var roomsJson = JsonSerializer.Deserialize<RoomJson[]>(json);
-        if (roomsJson == null)
-            return Task.FromResult(Array.Empty<Room>());
+	const string Url = "http://localhost:8080/v1/rooms";
+	const string UrlRandom = "http://localhost:8080/v1/rooms_random";
 
-        int size = roomsJson.Length;
-        var res = new Room[size];
-        for (int i = 0; i < size; ++i)
-        {
-            res[i] = new Room(roomsJson[i]);
-        }
-        return Task.FromResult(res);
-    }
-    
-    public Task<Room> GetRoom(int id)
-    {
-        string json = new HttpClient().GetStringAsync($"{Url}({id})").Result;
-        var roomsJson = JsonSerializer.Deserialize<RoomJson>(json) ?? new RoomJson();
-        return Task.FromResult(new Room(roomsJson));
-    }
-}
+	public Task<Room[]> GetRooms(bool random = false)
+	{
+		string json = new HttpClient().GetStringAsync(random ? UrlRandom : Url).Result;
+
+		var roomsJson = JsonSerializer.Deserialize<RoomJson[]>(json);
+		if (roomsJson == null)
+			return Task.FromResult(Array.Empty<Room>());
+
+		int size = roomsJson.Length;
+		var res = new Room[size];
+		for (int i = 0; i < size; ++i)
+		{
+			res[i] = new Room(roomsJson[i]);
+		}
+
+		return Task.FromResult(res);
+	}
+
+	public Task<Room> GetRoom(int id)
+	{
+		string json = new HttpClient().GetStringAsync($"{Url}({id})").Result;
+		var roomsJson = JsonSerializer.Deserialize<RoomJson>(json) ?? new RoomJson();
+		return Task.FromResult(new Room(roomsJson));
+	}
 }
