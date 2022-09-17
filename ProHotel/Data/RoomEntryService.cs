2d8581ae8a780c2bf44e1ea3137ab31ef23cea06
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-09-17 11:27:38 +0200

    Uwzględnienie sezonów

--- ProHotel/Data/RoomEntryService.cs
+++ ProHotel/Data/RoomEntryService.cs
@@ -7,12 +7,29 @@ public class RoomEntryService
 {
 	const string Url = "http://localhost:8080/v1/rooms";
 	const string UrlRandom = "http://localhost:8080/v1/rooms_random";
-    
+	private SeasonJSON[] seasons;
+	private int actualDiscount = 0;
+
+	public RoomEntryService()
+	{
+		int actualMonth = DateTime.Now.Month;
+		SeasonJSON? actualSeason = null;
+		seasons = GetSeasons();
+		foreach (var t in seasons)
+		{
+			if (actualMonth >= t.to.Month) continue;
+			actualSeason = t;
+			break;
+		}
+
+		actualDiscount = actualSeason?.price ?? 0;
+	}
+
 	public Task<Room[]> GetRooms(bool random = false)
 	{
 		string json = new HttpClient().GetStringAsync(random ? UrlRandom : Url).Result;
 
-        var roomsJson = JsonSerializer.Deserialize<RoomJson[]>(json);
+		var roomsJson = JsonSerializer.Deserialize<RoomJson[]>(json);
 		if (roomsJson == null)
 			return Task.FromResult(Array.Empty<Room>());
 
@@ -21,6 +38,7 @@ public class RoomEntryService
 		for (int i = 0; i < size; ++i)
 		{
 			res[i] = new Room(roomsJson[i]);
+			res[i].PricePerDay *= (100.0f + actualDiscount) / 100.0f;
 		}
 
 		return Task.FromResult(res);
@@ -33,27 +51,41 @@ public class RoomEntryService
 		return Task.FromResult(new Room(roomsJson));
 	}
 
-    public Task<Room[]> GetRooms(SearchRoomJSON searchRoomJson)
-    {
-        using var client = new HttpClient();
-        Console.WriteLine("4: " + searchRoomJson);
-        var endpoint = new Uri("http://localhost:8080/v1/free");
-        var newPostJson = JsonSerializer.Serialize(searchRoomJson);
+	public Task<Room[]> GetRooms(SearchRoomJSON searchRoomJson)
+	{
+		using var client = new HttpClient();
+		Console.WriteLine("4: " + searchRoomJson);
+		var endpoint = new Uri("http://localhost:8080/v1/free");
+		var newPostJson = JsonSerializer.Serialize(searchRoomJson);
 
-        var payLoad = new StringContent(newPostJson, Encoding.UTF8, "application/json");
-        var json = client.PostAsync(endpoint, payLoad).Result.Content.ReadAsStringAsync().Result;
-            
-        var roomsJson = JsonSerializer.Deserialize<RoomJson[]>(json);
-        if (roomsJson == null)
-            return Task.FromResult(Array.Empty<Room>());
+		var payLoad = new StringContent(newPostJson, Encoding.UTF8, "application/json");
+		var json = client.PostAsync(endpoint, payLoad).Result.Content.ReadAsStringAsync().Result;
 
-        int size = roomsJson.Length;
-        var res = new Room[size];
-        for (int i = 0; i < size; ++i)
-        {
-            res[i] = new Room(roomsJson[i]);
-        }
+		var roomsJson = JsonSerializer.Deserialize<RoomJson[]>(json);
+		if (roomsJson == null)
+			return Task.FromResult(Array.Empty<Room>());
 
-        return Task.FromResult(res);
-    }
+		int size = roomsJson.Length;
+		var res = new Room[size];
+		for (int i = 0; i < size; ++i)
+		{
+			res[i] = new Room(roomsJson[i]);
+			res[i].PricePerDay *= (100.0f + actualDiscount) / 100.0f;
+		}
+
+		return Task.FromResult(res);
+	}
+
+	public SeasonJSON[] GetSeasons()
+	{
+		using var client = new HttpClient();
+		var endpoint = new Uri("http://localhost:8080/v1/seasons");
+
+		var json = client.GetStringAsync(endpoint).Result;
+
+		var seasonsJson = JsonSerializer.Deserialize<SeasonJSON[]>(json);
+		if (seasonsJson == null)
+			return Array.Empty<SeasonJSON>();
+		return seasonsJson;
+	}
 }
---
+++ ProHotel/Data/SeasonJSON.cs
@@ -0,0 +1,7 @@
+public class SeasonJSON
+{
+    public DateTime from { get; set; }
+    public string name { get; set; }
+    public int price { get; set; }
+    public DateTime to { get; set; }
+}
